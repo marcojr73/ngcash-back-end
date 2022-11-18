efba2de911834d0ba0d653d95d088215110af1f6
marcojr73 <marcojuniorengineer@gmail.com>
2022-11-18 14:03:51 -0300

    account integration tests

---
+++ testes/integration/account.test.ts
@@ -0,0 +1,30 @@
+import supertest from "supertest"
+import { prisma } from "../../src/config/database.js"
+import app from "../../src/app.js"
+import dotenv from "dotenv"
+import * as factory from "../factory/factory.js"
+
+dotenv.config()
+
+console.log("tests running on base" + process.env.DATABASE_URL)
+
+beforeEach(async ()=> {
+    await prisma.users.deleteMany({where: {}})
+    await prisma.accounts.deleteMany({where: {}})
+})
+
+describe("account", ()=> {
+    it("should return details account", async () => {
+        const accountId = await factory.createUser()
+        const token = factory.generateTokenTest(accountId)
+        const response = await supertest(app).get("/account").auth(token, {type: "bearer"})
+        console.log(response.body)
+        expect(response.statusCode).toBe(200)
+    })
+
+    it("should fail to send incorrect token", async () => {
+        const token = factory.generateIncorrectTokenTest()
+        const response = await supertest(app).get("/account").auth(token, {type: "bearer"})
+        expect(response.statusCode).toBe(401)
+    })
+})
